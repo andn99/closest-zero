7549f657e9b7cc91d15e63890551ec68b80690c7
Ngoc An Do <anngdo@gmail.com>
2021-03-25 09:42:58 +1030

    Improve performance by adding and removing items in sublist

--- src/main/java/com/daitum/hiring/test/closestzero/service/ClosestZeroService.java
+++ src/main/java/com/daitum/hiring/test/closestzero/service/ClosestZeroService.java
@@ -1,5 +1,6 @@
 package com.daitum.hiring.test.closestzero.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -24,28 +25,51 @@ public class ClosestZeroService {
     if (list == null || list.isEmpty() || n <= 0 || n > list.size())
       return -1;
 
-    // Set the initial values for minIndex and closestSum
+    // The temporary variable for holding the index of the first element of the
+    // sublist whose sum is closest to zero
     Integer minIndex = 0;
-    Integer closestSum = Integer.MAX_VALUE;
+    // The current index used for the while loop
+    Integer currentIndex = 0;
 
     /**
-     * Note that the performance of using for loop here is still the same as getting
-     * the sub-list of the first n items, then gradually remove the fist item of the
-     * sub-list and add the next item to the end of that sub-list.
-     * 
-     * Big O: O((list.size()-n)*n)
+     * The first sub-list of n items. Create a new ArrayList so that it can be
+     * mutable for removing and adding item. If we use List<Integer> subList =
+     * list.subList(0, n) and the list is a fixed size list param (ie, created by
+     * Arrays.asList(1,2,3)). Then the subList might become immutable and we'd get
+     * error when using subList.remove(), subList.add()
      */
 
-    for (Integer i = 0; i <= list.size() - n; i++) {
-      List<Integer> subList = list.subList(i, i + n);
-      Integer sum = absSumList(subList);
-      // If sum is 0, return i immediately as it's the index of the first element
-      if (sum == 0)
-        return i;
+    List<Integer> subList = new ArrayList<>();
+    subList.addAll(list.subList(0, n));
 
-      if (sum < closestSum) {
-        closestSum = sum;
-        minIndex = i;
+    // The sum of the current subList
+    Long currentSum = sumList(subList);
+    // If current sum is 0, the first sublist is the one whose sum is closest to 0
+    if (currentSum == 0)
+      return 0;
+
+    Long closestSum = Math.abs(currentSum);
+
+    while (currentIndex < list.size() - n) {
+      currentIndex++;
+      // Remove the first item in the sublist
+      Integer firstItem = subList.remove(0);
+
+      // Add the next item to the sublist
+      Integer nextItem = list.get(currentIndex + n - 1);
+      subList.add(nextItem);
+
+      // Re-calculate the sum of current subList by adding the next item and
+      // substracting the removed item
+      currentSum = currentSum + nextItem - firstItem;
+
+      // Return current index if sum is 0, don't need to consider the next sub-lists
+      if (currentSum == 0)
+        return currentIndex;
+
+      if (Math.abs(currentSum) < closestSum) {
+        closestSum = Math.abs(currentSum);
+        minIndex = currentIndex;
       }
     }
 
@@ -53,16 +77,16 @@ public class ClosestZeroService {
   }
 
   /**
-   * Return the absolute value of the sum of the list
+   * Return the the sum of all items in the list
    * 
    * @param list
-   * @return absSum
+   * @return sum
    */
-  private Integer absSumList(List<Integer> list) {
-    Integer sum = 0;
+  private Long sumList(List<Integer> list) {
+    Long sum = 0L;
     for (Integer item : list) {
       sum += item;
     }
-    return Math.abs(sum);
+    return sum;
   }
 }
